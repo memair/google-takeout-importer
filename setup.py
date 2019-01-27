8e06120d6d5b506bc1a945f49234d2d2072baa92
gregology <greg@gho.st>
2019-01-27 11:35:39 -0500

    fixing new order by

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='gtmem',
-  version='0.3',
+  version='0.4',
   description='Google Takeout importer for Memair',
   long_description=open('README.rst').read(),
   url='https://github.com/memair/google-takeout-impoter',
